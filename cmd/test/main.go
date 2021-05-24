0ace8d29cfcfc23fa83fbedefbe296ea248f9ea3
Karol.Qu <quhongbin@ideapool.tv>
2021-05-24 15:10:08 +0800

    fork code & modify go.mod

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -9,7 +9,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/kjk/lzmadec"
+	"github.com/wowqhb/lzmadec"
 )
 
 func usageAndExit() {
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
-module github.com/kjk/lzmadec
+module github.com/wowqhb/lzmadec
+
+go 1.15
